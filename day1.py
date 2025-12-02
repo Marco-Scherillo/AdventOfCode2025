eb136ce32bac752dced5a34c069624788bd80215
root <root@DESKTOP-H0G33IQ.localdomain>
2025-12-02 00:36:14 -0500

    this mod was ass i could not figure out the math but i just brute forced it

--- day1.py
+++ day1.py
@@ -2,25 +2,43 @@
 The actual password is the number of times the dial is 
 left pointing at 0 after any rotation in the sequence.
 '''
+import math
 
 #read file
 dial = 50 #start at 50 
-counter = 0
+hits_zero = 0
+passes_zero = 0
 with open('./inputday1.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
-        move = line.strip()
-        direction = ""
-        value = ""
-        for char in move:
-            if char.isalpha():
-                direction = char
-            elif char.isdigit():
-                value+= char
+        m = line.strip()
+        direction = m[0]
+        x = int(m[1:])
+
         if direction == "L":
-            dial = (dial - int(value)) % 100 #use mod to loop so 99 goes back to 0 and vice versa.
+            signed_x = -x 
         elif direction == "R":
-            dial = (dial + int(value)) % 100
+            signed_x = x
+        
+
+        start = dial
+        end_raw = start + signed_x
+        
+        #calculate how many time we pass 
+        #imma brute force this bitch 
+        current = start
+        step = 1 if direction == "R" else -1
+        for _ in range(x):
+            current =(current + step ) % 100
+            if current == 0:
+                passes_zero+=1
+    
+        dial = end_raw % 100 #use mod to loop so 99 goes back to 0 and vice versa.
+        
         if dial == 0:
-            counter+=1
-    print(counter)
+            hits_zero+=1
+
+
+        
+    print(hits_zero)
+    print(passes_zero)
